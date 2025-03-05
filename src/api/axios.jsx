5b771be129f4cb6c4e2d946348ab1e4cc20a36c4
Paula1samir <bola.samircstech20@gmail.com>
2025-03-05 16:21:05 +0200

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export default axios.create({
-    baseURL: 'https://bulkify-back-end.vercel.app/api/v1/customers/login'
+    baseURL: 'https://bulkify-back-end.vercel.app'
     });
