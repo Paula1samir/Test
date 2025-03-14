f1976b7986b014583f0e01922377e0312fce54cc
Paula1samir <bola.samircstech20@gmail.com>
2025-03-14 18:37:49 +0200

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import "./Login.css"
-import "./SignUp.css"
 import "./Login"
 import ForgetPassword from "./forgetPass/forgetPassword"
 import "bootstrap/dist/css/bootstrap.min.css";
