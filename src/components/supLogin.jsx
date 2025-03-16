d10fb14bbaece791fbe6c07de61cf423123820e3
Paula1samir <bola.samircstech20@gmail.com>
2025-03-16 23:22:50 +0200

    Update supLogin.jsx

--- src/components/supLogin.jsx
+++ src/components/supLogin.jsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState, useEffect, useContext } from "react";
-import "../Login.css";
-import SupForgetPassword from "./supForgetPassword.jsx";
-import SupSignUp from "./supSignUp.jsx";
-import AuthContext from "../context/AuthProvider.jsx";
+import "./Login.css";
+import SupForgetPassword from "./Supplier/supForgetPassword.jsx";
+import SupSignUp from "./Supplier/supSignUp.jsx";
+import AuthContext from "./context/AuthProvider.jsx";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
